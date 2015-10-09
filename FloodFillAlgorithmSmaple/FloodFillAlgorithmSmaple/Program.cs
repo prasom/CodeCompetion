130a5e18e1375c584b1732a05ccce743a748a141
Pornsukp <pornsuk.prasom@thomsonreuters.com>
2015-10-09 18:38:18 +0700

    - First Commit

---
+++ FloodFillAlgorithmSmaple/FloodFillAlgorithmSmaple/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloodFillAlgorithmSmaple
+{
+    class Program
+    {
+        private const int M = 8;
+        private const int N = 8;
+
+        static void Main(string[] args)
+        {
+            int[,] screen = new int[M, N];
+            screen = new int[,]
+            {
+                {1, 1, 1, 1, 1, 1, 1, 1},
+                {1, 1, 1, 1, 1, 1, 0, 0},
+                {1, 0, 0, 1, 1, 0, 1, 1},
+                {1, 2, 2, 2, 2, 0, 1, 0},
+                {1, 1, 1, 2, 2, 0, 1, 0},
+                {1, 1, 1, 2, 2, 2, 2, 0},
+                {1, 1, 1, 1, 1, 2, 1, 1},
+                {1, 1, 1, 1, 1, 2, 2, 1}
+            };
+
+            int x = 4, y = 4, newC = 3;
+            FloodFill(screen, x, y, newC);
+
+            Console.WriteLine("Updated screen after call to floodFill: \n");
+
+            for (int i = 0; i < M; i++)
+            {
+                for (int j = 0; j < N; j++)
+                    Console.Write(screen[i, j] + " ");
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
+        }
+
+        private static void FloodFill(int[,] screen, int x, int y, int newC)
+        {
+            int prevC = screen[x, y];
+            Console.WriteLine(prevC);
+            floodFillUtil(screen, x, y, prevC, newC);
+        }
+
+        static void floodFillUtil(int[,] screen, int x, int y, int prevC, int newC)
+        {
+            // Base cases
+            if (x < 0 || x >= M || y < 0 || y >= N)
+                return;
+            if (screen[x, y] != prevC)
+                return;
+
+            // Replace the color at (x, y)
+            screen[x, y] = newC;
+
+            // Recur for north, east, south and west
+            floodFillUtil(screen, x + 1, y, prevC, newC);
+            floodFillUtil(screen, x - 1, y, prevC, newC);
+            floodFillUtil(screen, x, y + 1, prevC, newC);
+            floodFillUtil(screen, x, y - 1, prevC, newC);
+        }
+    }
+}
---
+++ SampleCodeChallenge/SampleCodeChallenge/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeChallenge1;
+
+namespace SampleCodeChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BattleshipBoard board = new BattleshipBoard();
+            TigerWarShip tigerWarShip = new TigerWarShip();
+            tigerWarShip.Play(board);
+            Console.WriteLine(tigerWarShip.GetTeamName());
+            Console.ReadLine();
+        }
+    }
+
+    class TigerWarShip : IBattleshipAi
+    {
+        public void Play(IFireable fireable)
+        {
+            int hit = 0;
+            for (int row = 1; row <= 10; row++)
+            {
+                for (int column = 1; column <= 10; column++)
+                {
+                    if (fireable.Fire(row, column) == (Result)ShotResult.HIT)
+                    {
+                        hit++;
+                        Console.WriteLine("Hit on col {0} row {1} and now total {2}", row, column, hit);
+                        if (hit == 17)
+                            continue;
+
+                    }
+                }
+            }
+        }
+
+        public string GetTeamName()
+        {
+            return "Liger";
+        }
+    }
+
+    enum ShotResult
+    {
+        HIT,
+        MISS,
+        MISSION_COMPLETED
+    }
+}
